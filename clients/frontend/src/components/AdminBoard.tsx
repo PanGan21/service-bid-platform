704d44cf74cd94a78ee266c0962ac764abbc2ce9
panos <panos@kryha.io>
2023-02-02 10:44:13 +0100

    frontend: add section for in progress requests

--- clients/frontend/src/components/AdminBoard.tsx
+++ clients/frontend/src/components/AdminBoard.tsx
@@ -6,9 +6,14 @@ type Props = {};
 
 export const AdminBoard: React.FC<Props> = () => {
   const [isPendingRequestsOpen, setPendingRequestsOpen] = useState(false);
+  const [isInProgressRequestsOpen, setInProgressRequestsOpen] = useState(false);
 
   const togglePendingRequests = () => {
     setPendingRequestsOpen(!isPendingRequestsOpen);
+  };
+
+  const toggleInProgressRequests = () => {
+    setInProgressRequestsOpen(!isInProgressRequestsOpen);
   };
 
   return (
@@ -26,6 +31,19 @@ export const AdminBoard: React.FC<Props> = () => {
       <div style={{ display: isPendingRequestsOpen ? "block" : "none" }}>
         Choose a request to resolve the winning bid!
         <PendingRequests />
+      </div>
+      <br />
+      <div onClick={toggleInProgressRequests} style={{ cursor: "pointer" }}>
+        <img
+          style={{ width: "50px", height: "60px" }}
+          src={request}
+          alt="profile-img"
+        />
+        <span>
+          <strong>In Progress Requests</strong>
+        </span>
+      </div>
+      <div style={{ display: isInProgressRequestsOpen ? "block" : "none" }}>
       </div>
     </div>
   );
